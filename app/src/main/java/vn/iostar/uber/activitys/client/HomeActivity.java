4ee02c0782fa09bf89050b356cc48b5dd20c1333
Khanh Huynh <hongkhanh2901@gmail.com>
2024-05-12 11:28:16 +0700

    đăng nhập băng homeactivity

--- app/src/main/java/vn/iostar/uber/activitys/client/HomeActivity.java
+++ app/src/main/java/vn/iostar/uber/activitys/client/HomeActivity.java
@@ -1,16 +1,120 @@
 package vn.iostar.uber.activitys.client;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthMethodPickerLayout;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import vn.iostar.uber.R;
+import vn.iostar.uber.activitys.MainActivityClient;
+import vn.iostar.uber.activitys.SplashScreenActivity;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+    private final static int LOGIN_REQUEST_CODE = 7171;
+    private List<AuthUI.IdpConfig> providers;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+    protected void onStart() {
+        super.onStart();
+        delaySplashScreen();
+    }
+
+    @Override
+    protected void onStop() {
+
+        if(firebaseAuth !=  null && listener != null){
+            firebaseAuth.removeAuthStateListener(listener);
+        }
+        super.onStop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        init();
+    }
+    private void init(){
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = myFirebaseAuth -> {
+            FirebaseUser user = myFirebaseAuth.getCurrentUser();
+            if(user != null){
+                Toast.makeText(HomeActivity.this, "Welcome"+user.getUid(), Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(HomeActivity.this, MainActivityClient.class));
+
+            }
+            else{
+                setContentView(R.layout.activity_home);
+                TextView btnLogin = findViewById(R.id.btn_login);
+                btnLogin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showLoginDialog();
+                    }
+                });
+            }
+        };
+
+    }
+
+    private void showLoginDialog() {
+        AuthMethodPickerLayout authMethodPickerLayout = new AuthMethodPickerLayout
+                .Builder(R.layout.layout_sign_in)
+                .setPhoneButtonId(R.id.btn_phone_sign_in)
+                .setGoogleButtonId(R.id.btn_google_sign_in)
+                .build();
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAuthMethodPickerLayout(authMethodPickerLayout)
+                .setIsSmartLockEnabled(false)
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(providers)
+                .build(),LOGIN_REQUEST_CODE);
+    }
+
+
+    @SuppressLint("CheckResult")
+    private void delaySplashScreen() {
+
+        Completable.timer(5, TimeUnit.SECONDS,
+                        AndroidSchedulers.mainThread())
+                .subscribe(() ->  firebaseAuth.addAuthStateListener(listener));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == LOGIN_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }
+            else{
+                Toast.makeText(this,"[ERROR]:" + response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
